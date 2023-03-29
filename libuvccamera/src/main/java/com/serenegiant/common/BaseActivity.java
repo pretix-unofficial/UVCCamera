989007b5040587182df98612ebcc8f8311b07c84
Maximilian Richt <richt@rami.io>
2023-03-29 16:25:22 +0200

    replace old android.support libs

--- libuvccamera/src/main/java/com/serenegiant/common/BaseActivity.java
+++ libuvccamera/src/main/java/com/serenegiant/common/BaseActivity.java
@@ -29,11 +29,12 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.serenegiant.dialog.MessageDialogFragmentV4;
 import com.serenegiant.utils.BuildCheck;
